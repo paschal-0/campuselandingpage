e9e3209d788356a354f9f548208e1417b4651c75
paschal-0 <132915311+paschal-0@users.noreply.github.com>
2024-10-08 16:08:57 -0700

    Add files via upload

---
+++ contacts.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+// Contact Schema
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Contact Model
+const Contact = mongoose.model('Contact', contactSchema);
+
+module.exports = Contact;
